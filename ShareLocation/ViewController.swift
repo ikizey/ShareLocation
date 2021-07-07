d81d11964f9b376f8a6b0c856de090ea1e4f5623
ikizey <ikizey@icloud.com>
2021-07-07 23:57:26 +0300

    button now shares information about user's location.

--- ShareLocation/ViewController.swift
+++ ShareLocation/ViewController.swift
@@ -11,7 +11,7 @@ import CoreLocation
 
 class ViewController: UIViewController {
 
-    
+    // MARK: - properties
     @IBOutlet var map: MKMapView!
     
     @IBOutlet var locationLabel: UILabel!
@@ -19,9 +19,27 @@ class ViewController: UIViewController {
     @IBOutlet var blurContainer: UIVisualEffectView!
     @IBOutlet var blurButtonContainer: UIVisualEffectView!
     
+    var pin = MKPointAnnotation()
+    
     let locationManager = CLLocationManager()
     
-    override func viewDidLoad() {
+    var lastLatitude: CLLocationDegrees?
+    var lastLongitude: CLLocationDegrees?
+    var lastCoordinate: CLLocationCoordinate2D? {
+        didSet {
+            lastLatitude = lastCoordinate?.latitude
+            lastLongitude = lastCoordinate?.longitude
+            
+            pin.coordinate = lastCoordinate!
+            updateLabel()
+        }
+    }
+    
+    
+    
+    // MARK: - setup
+        
+    override func viewWillAppear(_ animated: Bool) {
         super.viewDidLoad()
         ///Setup blur containers
         blurContainer.layer.cornerRadius = 25
@@ -34,46 +52,52 @@ class ViewController: UIViewController {
         
         locationManager.requestWhenInUseAuthorization()
         locationManager.startUpdatingLocation()
-    }
-    
-    func render(_ location: CLLocation) {
-        let coordinate = CLLocationCoordinate2D(latitude: location.coordinate.latitude,
-                                                longitude: location.coordinate.longitude)
-        let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
-        let region = MKCoordinateRegion(center: coordinate, span: span)
-        map.setRegion(region, animated: true)
         
-        addPin(at: coordinate)
-        updateLocationLabel(with: coordinate)
+        map.addAnnotation(pin) //?
     }
     
-    func addPin(at coordinate: CLLocationCoordinate2D) {
-        let pin = MKPointAnnotation()
-        pin.coordinate = coordinate
-        map.addAnnotation(pin)
+    // MARK: - render location
+    
+    func render() {
+        let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        let region = MKCoordinateRegion(center: lastCoordinate!, span: span)
+        map.setRegion(region, animated: true)
     }
     
-    func updateLocationLabel(with coordinate: CLLocationCoordinate2D) {
+    func updateLabel() {
         let firstLine = "My location:\n"
-        let secondLine = String(format: "Latitude: %.2f\n", coordinate.latitude)
-        let thirdLine = String(format: "Longtitude: %2.f", coordinate.longitude)
+        let secondLine = String(format: "Latitude: %.2f\n", lastLatitude!)
+        let thirdLine = String(format: "Longtitude: %2.f", lastLongitude!)
         let text = firstLine + secondLine + thirdLine
         locationLabel.text = text
     }
     
     @IBAction func shareButtonTapped(_ sender: UIButton) {
-        
+        locationManager.startUpdatingLocation()
+        locationManager.requestLocation()
+        guard lastCoordinate != nil else { return }
+        let text = locationLabel.text!
+        let vc = UIActivityViewController(activityItems: [text], applicationActivities: [])
+        vc.popoverPresentationController?.sourceView = blurButtonContainer
+        present(vc, animated: true)
     }
-    
 }
+
+// MARK: - Location Manager Delegate
 
 extension ViewController: CLLocationManagerDelegate {
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        if let location = locations.first {
-            manager.startUpdatingLocation()
+        if let location = locations.last {
+            manager.stopUpdatingLocation()
             
-            render(location)
+            lastCoordinate = CLLocationCoordinate2D(latitude: location.coordinate.latitude,
+                                                    longitude: location.coordinate.longitude)
+            render()
         }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        guard error != nil else { return }
     }
 }
